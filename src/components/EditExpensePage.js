611b1474fe66000fd09ae010d531497e4d0f2344
Odd A Hartvigsen <padda76@users.noreply.github.com>
2017-12-08 15:10:59 +0100

    Added startEdit

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import ExpenseForm from './ExpenseForm';
-import { editExpense, startRemoveExpense } from '../actions/expenses';
+import { startEditExpense, startRemoveExpense } from '../actions/expenses';
 
 export class EditExpensePage extends React.Component {
 
     onSubmit = (expense) => {
-        this.props.editExpense(this.props.expense.id, expense);
+        this.props.startEditExpense(this.props.expense.id, expense);
         this.props.history.push('/');
     };
     onClick = () => {
@@ -28,7 +28,7 @@ export class EditExpensePage extends React.Component {
 }
   
 const mapDispatchToProps = (dispatch, props) => ({
-    editExpense : (id, expense) => dispatch(editExpense(id,expense)),
+    startEditExpense : (id, expense) => dispatch(startEditExpense(id,expense)),
     startRemoveExpense : (id) => dispatch(startRemoveExpense(id))
 });
 

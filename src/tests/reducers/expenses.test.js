8a77697d1d0a1a5cc22e0487d4cc6345c5203193
Odd A Hartvigsen <padda76@users.noreply.github.com>
2017-12-08 13:10:03 +0100

    Fetching data from database

--- src/tests/reducers/expenses.test.js
+++ src/tests/reducers/expenses.test.js
@@ -63,3 +63,13 @@ test('should not edit an expense when invalid id', () => {
     const state = expensesReducer(expenses,action);
     expect(state).toEqual(expenses);
 });
+
+test('should set expenses', () => {
+    //const expenses = expenses;
+    const action = {
+        type: 'SET_EXPENSES',
+        expenses: [expenses[1]]
+    };
+    const state = expensesReducer(expenses,action);
+    expect(state).toEqual([expenses[1]]);
+})
